fcc12222c2fdf480208c8a8c40cc77571b721ff9
jeppeter <jeppeter@gmail.com>
2023-07-27 15:01:32 +0800

    to add hashsize in ecvfybase

--- simplerun.sh
+++ simplerun.sh
@@ -31,9 +31,9 @@ then
 	pushd $PWD && cd /mnt/zdisk/clibs/test/ssltst && make && popd
 fi
 /mnt/zdisk/clibs/test/ssltst/ssltst ecgen --ecpriv $ecfile --ecpub $ecpub $ectype $privnum 2>$genlog
-/mnt/zdisk/clibs/test/ssltst/ssltst ecpubload $ectype  $ecpub 2>$outfile
-#/mnt/zdisk/clibs/test/ssltst/ssltst ecsignbase -o $signfile $ecfile $hashnumber $hashsize 2>$outfile
-#/mnt/zdisk/clibs/test/ssltst/ssltst ecvfybase $ectype $ecpub $hashnumber $signfile 2>$outfile
+#/mnt/zdisk/clibs/test/ssltst/ssltst ecpubload $ectype  $ecpub 2>$outfile
+/mnt/zdisk/clibs/test/ssltst/ssltst ecsignbase -o $signfile $ecfile $hashnumber $hashsize 2>$signlog
+/mnt/zdisk/clibs/test/ssltst/ssltst ecvfybase $ectype $ecpub $hashnumber $signfile $hashsize 2>$outfile
 
 python /mnt/zdisk/pylib/utils.py filterlog -i $outfile -o $simpleout python
 numbers=`cat $simpleout | grep -e 'random number' | awk '{print $3}' | xargs -I {} echo -n " {}"`
